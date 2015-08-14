ee6f9b45dedf27f45c0c79d419a6e03bcf4de856
Tomoyuki Kano <tomo@appletz.jp>
2015-08-14 09:35:07 +0000

    fix

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -4,26 +4,25 @@ class OwnershipsController < ApplicationController
   def create
     if params[:asin]
       @item = Item.find_or_initialize_by(asin: params[:asin])
+      # itemsテーブルに存在しない場合はAmazonのデータを登録する。
+      if @item.new_record?
+        begin
+          response =  Amazon::Ecs.item_lookup(params[:asin] , response_group: 'Medium' , country: 'jp')
+        rescue Amazon::RequestError => e
+          return render :js => "alert('#{e.message}')"
+        end
+
+        amazon_item       = response.items.first
+        @item.title        = amazon_item.get('ItemAttributes/Title')
+        @item.small_image  = amazon_item.get("SmallImage/URL")
+        @item.medium_image = amazon_item.get("MediumImage/URL")
+        @item.large_image  = amazon_item.get("LargeImage/URL")
+        @item.detail_page_url = amazon_item.get("DetailPageURL")
+        @item.raw_info        = amazon_item.get_hash
+        @item.save!
+      end
     else
       @item = Item.find(params[:item_id])
-    end
-
-    # itemsテーブルに存在しない場合はAmazonのデータを登録する。
-    if @item.new_record?
-      begin
-        response =  Amazon::Ecs.item_lookup(params[:asin] , response_group: 'Medium' , country: 'jp')
-      rescue Amazon::RequestError => e
-        return render :js => "alert('#{e.message}')"
-      end
-
-      amazon_item       = response.items.first
-      @item.title        = amazon_item.get('ItemAttributes/Title')
-      @item.small_image  = amazon_item.get("SmallImage/URL")
-      @item.medium_image = amazon_item.get("MediumImage/URL")
-      @item.large_image  = amazon_item.get("LargeImage/URL")
-      @item.detail_page_url = amazon_item.get("DetailPageURL")
-      @item.raw_info        = amazon_item.get_hash
-      @item.save!
     end
 
     if params[:type] == "Have"
