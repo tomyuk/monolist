75e9cc063ee4f88df4745b6b3f0ffa966e307866
Tomoyuki Kano <tomo@appletz.jp>
2015-08-16 07:27:34 +0000

    fix ranking controller

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,13 +1,15 @@
 class RankingController < ApplicationController
   def have
-    ranking = Have.group(:item_id).order('count_item_id desc').count(:item_id)
-    ids = ranking.collect{|k,v| k }
+#    ranking = Have.group(:item_id).order('count_item_id desc').count(:item_id)
+#    ids = ranking.collect{|k,v| k }
+    ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
     @items = Item.find(ids)
   end
 
   def want
-    ranking = Want.group(:item_id).order('count_item_id desc').count(:item_id)
-    ids = ranking.collect{|k,v| k }
+#    ranking = Want.group(:item_id).order('count_item_id desc').count(:item_id)
+#    ids = ranking.collect{|k,v| k }
+    ids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
     @items = Item.find(ids)
   end
 end
