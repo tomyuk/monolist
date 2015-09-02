6f39d890c603d48ac8b44785d8c4a5f6b4e0ecb1
Tomoyuki Kano <tomo@appletz.jp>
2015-09-02 23:25:51 +0000

    update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :logged_in_user , except: [:show]
+  before_action :logged_in_user, except: [:show]
   before_action :set_item, only: [:show]
 
   def new
