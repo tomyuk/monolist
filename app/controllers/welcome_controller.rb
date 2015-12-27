016490a13675abe8f5805da4771695fd2204726a
Tomoyuki Kano <tomo@appletz.jp>
2015-12-27 01:40:07 +0000

    add javascript to message-submit button. And fix f.select :class

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     # @items = Item.all.order("updated_at DESC").page(params[:page]).per(30)
-    @items = Item.all.order(:asin).page(params[:page]).per(30)
+    @items = Item.all.order(:asin).page(params[:page]).per(32)
   end
 end
