4013145ae7189bacfc5eeb5b8ba18411d1bf3c34
Tomoyuki Kano <tomo@appletz.jp>
2015-12-27 01:49:28 +0000

    update welcome page query

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     # @items = Item.all.order("updated_at DESC").page(params[:page]).per(30)
-    @items = Item.all.order(:asin).page(params[:page]).per(32)
+    @items = Item.all.joins('LEFT OUTER JOIN ownerships ON items.id = ownerships.item_id').group('items.id').order(:asin).page(params[:page]).per(32)
   end
 end
