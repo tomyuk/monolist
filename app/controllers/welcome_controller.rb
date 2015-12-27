3444263d8ca59c35e809a31b7f348ec5cdba1441
Tomoyuki Kano <tomo@appletz.jp>
2015-12-27 01:06:38 +0000

    add pagination into the Welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @items = Item.all.order("updated_at DESC").limit(30)
+    @items = Item.all.order("updated_at DESC").page(params[:page]).per(30)
   end
 end
