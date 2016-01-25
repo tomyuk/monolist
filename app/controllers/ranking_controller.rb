1e804b482d40941773c54a5d55f7b27e6c612106
Tomoyuki KANO <tomo@appletz.jp>
2016-01-25 17:19:27 +0900

    fix ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,15 +1,13 @@
 class RankingController < ApplicationController
   def have
-    # ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-    # ids = Have.joins(:item).group(:item_id).order('count_item_id desc, max(items.created_at) desc').limit(10).count(:item_id).keys
-    ids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @counts = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    ids = @counts.keys
     @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
-    @counts = ids
   end
 
   def want
-    ids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
+    @counts = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    ids = @counts.keys
     @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
-    @counts = ids
   end
 end
